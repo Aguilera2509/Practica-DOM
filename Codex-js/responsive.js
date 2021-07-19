8c75da6b2b81f8a697f81cf4e62ecdd7e7405cf1
José Aguilera <ja846699@gmail.com>
2021-07-19 12:25:54 -0400

    Aprendiendo mas

---
+++ Codex-js/responsive.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded",(e)=>{
+    
+    let ancho = window.innerWidth;
+    let altura = window.innerHeight;
+
+    const $video = document.getElementById('video'),
+        $mapa = document.getElementById('map');
+     
+    if((ancho >= 738) && (altura >= 510)){
+        $video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/XPzCY7NDuE0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    }else{
+        $video.innerHTML = `<a href="https://www.youtube.com/watch?v=XPzCY7NDuE0&t=1s" target="_blank">Ver vídeo</a>`;
+    }
+
+    if((ancho >= 738) && (altura >= 510)){
+        $mapa.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d6303592.624861041!2d-103.67269200000001!3d39.516157873902465!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sve!4v1626711241268!5m2!1ses!2sve" width="560" height="315" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`;
+    }else{
+        $mapa.innerHTML = `<a href="https://www.google.com/maps/@39.5161579,-103.672692,6z?hl=es" target="_blank">Ver mapa</a>`;
+    }
+
+})
--- Codex-js/scrollTop.js
+++ Codex-js/scrollTop.js
@@ -1,4 +1,5 @@
-const w = window;
+const w = window,
+    miStorage = localStorage;
 
 w.addEventListener("scroll", (e) =>{
     
@@ -17,30 +18,38 @@ w.addEventListener("scroll", (e) =>{
 
 //------------------------------ MODO OSCURO ------------------------------------
 
-document.addEventListener("click",(e) =>{
 
+document.addEventListener("click",(e) =>{
     const $body = document.getElementById('cuerpo'),
         $dataDark = document.querySelectorAll("[data-dark]"),
         $icono = document.getElementById('oscuro');
-
+    
     let moon = "🌙",
         sun = "☀";
+    
+    const darkMode = () =>{
+        $body.classList.add('oscuro-body');
+        $dataDark.forEach((el) => el.classList.add('titulos-visibles'));
+        $icono.textContent = sun;
+        miStorage.setItem("theme","dark");
+    }
+    
+    const lightMode = () =>{
+        $body.classList.remove('oscuro-body');
+        $dataDark.forEach((el) => el.classList.remove('titulos-visibles'));
+        $icono.textContent = moon;
+        miStorage.setItem("theme","light");
+    }
 
     if(e.target.matches('.modo-oscuro')){
 
         if($icono.textContent === moon){
-            $body.classList.add('oscuro-body');
-            $dataDark.forEach((el) => el.classList.add('titulos-visibles'));
-            $icono.textContent = sun;
+            darkMode();
+        }else{
+            lightMode();
         }
-        else{
-            $body.classList.remove('oscuro-body');
-            $dataDark.forEach((el) => el.classList.remove('titulos-visibles'));
-            $icono.textContent = moon;
-        }
-
     }
-
+    
 
 //---------------------------- MENU DE HAMBURGUESA ------------------------------
 
@@ -48,11 +57,41 @@ document.addEventListener("click",(e) =>{
     const $btnRed = document.getElementById('btn-activator'),
         $secciones = document.getElementById('secciones');
 
-    if(e.target.matches('#btn-activator') || e.target.matches('.hamburger-box')){
+    if((e.target.matches('#btn-activator')) || (e.target.matches('.hamburger-inner')) || (e.target.matches('.hamburger-box'))){
         $btnRed.classList.toggle('is-active');
         $secciones.classList.toggle('is-active');
     }
 
+    if(e.target.matches("[data-link]")){
+        $btnRed.classList.remove('is-active');
+        $secciones.classList.remove('is-active');
+    }
     
 
 })
+
+document.addEventListener("DOMContentLoaded",(e)=>{
+
+    const $body = document.getElementById('cuerpo'),
+    $dataDark = document.querySelectorAll("[data-dark]"),
+    $icono = document.getElementById('oscuro');
+
+    const darkMode = () =>{
+        $body.classList.add('oscuro-body');
+        $dataDark.forEach((el) => el.classList.add('titulos-visibles'));
+        $icono.textContent = sun;
+        miStorage.setItem("theme","dark");
+    }
+
+    const lightMode = () =>{
+        $body.classList.remove('oscuro-body');
+        $dataDark.forEach((el) => el.classList.remove('titulos-visibles'));
+        $icono.textContent = moon;
+        miStorage.setItem("theme","light");
+    }
+
+    if(miStorage.getItem("theme") === null) miStorage.setItem("theme","light");
+    if(miStorage.getItem("theme") === "light") lightMode();
+    if(miStorage.getItem("theme") === "dark") darkMode();
+})
+
