1a7226067ca8a3d5d3b9c99f47480e9a75709ca5
José Aguilera <ja846699@gmail.com>
2021-08-04 13:03:35 -0400

    Finish

---
+++ Codex-js/reader.js
@@ -0,0 +1,51 @@
+let voices = [];
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    
+    const $speechSelect = document.getElementById('speech-select'),
+        message = new SpeechSynthesisUtterance();
+
+    window.speechSynthesis.addEventListener("voiceschanged", (e)=>{
+
+        voices = speechSynthesis.getVoices();
+
+        console.log(voices);
+
+        voices.forEach((voice) =>{
+            const $option = document.createElement('option');
+
+            $option.value = voice.name;
+            $option.textContent = `${voice.name} ---- ${voice.lang}`;
+
+            $speechSelect.appendChild($option);
+        })
+    })
+
+    document.addEventListener("change",(e)=>{
+    
+        if(e.target === $speechSelect){
+            message.voice = voice.find(voice => voice.name === e.target.value);
+        }
+    
+    })
+})
+
+
+document.addEventListener("click",(e)=>{
+
+    const $btn = document.getElementById('voz-btn');
+
+    if(e.target.matches('#voz-btn')){
+
+        const $value_input = document.getElementById('leer').value;
+
+        speechSynthesis.speak(new SpeechSynthesisUtterance($value_input));
+
+        $btn.disabled = true;
+
+        setTimeout(()=>{
+            $btn.disabled = false;
+        }, 3000);
+    }
+
+})
