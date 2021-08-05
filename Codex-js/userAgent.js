e80039219dd324289474c05626018d94d0d6322b
José Aguilera <ja846699@gmail.com>
2021-08-05 11:52:41 -0400

    Agregando reconocimiento de voz

--- Codex-js/userAgent.js
+++ Codex-js/userAgent.js
@@ -1,3 +1,5 @@
+let rec;
+
 document.addEventListener("DOMContentLoaded",(e)=>{
 
     const $generalInfo = document.getElementById('user-agent');
@@ -40,4 +42,35 @@ document.addEventListener("DOMContentLoaded",(e)=>{
     $generalInfo.innerHTML += `<h4>Plataforma: <b>${isMobile.any()? isMobile.any():isDesktop.any()}</b></h4>`;
     $generalInfo.innerHTML += `<h4>Navegador: <b>${isBrowser.any()}</b></h4>`;
 
+
+    document.addEventListener("click", (e)=>{
+    
+        if(e.target.matches('#reconocimiento-voz-btn')){
+            
+            if((isBrowser.chrome()) || (isBrowser.safari()) || (isBrowser.edge())){
+    
+                rec = new webkitSpeechRecognition();
+                rec.lang = "es-AR";
+                rec.continuous = true;
+                rec.interim = true;
+                rec.addEventListener("result", iniciar);
+    
+                rec.start();
+                document.getElementById('reconocimiento-voz-btn').disabled = true;
+            }else{
+                alert("Lo siento si no estas en Chrome o safari o edge no se puede correr este código, el nagevador no lo soporta");
+            }
+    
+        }
+    
+    })
+    
 })
+
+function iniciar(event){
+    for(i = event.resultIndex; i < event.results.length; i++){
+        document.getElementById('leer').value = event.results[i][0].transcript;   
+    }
+    rec.stop();
+    document.getElementById('reconocimiento-voz-btn').disabled = false;
+}
